dbab319b948de25337a6c6dcbd8609fa42116e18
zhangtong <840043122@qq.com>
2019-05-24 18:06:11 +0800

    change testnet

--- .history/src/windows/controllers/index_20190524162707.js
+++ .history/src/windows/controllers/index_20190524162707.js
@@ -225,40 +225,38 @@ class Index {
           }
       
       }]
-    }
-    // ,{
-    //   label: s10,
-    //   submenu: [{
-    //     label: s11, type: 'checkbox',id:'check1',checked: checka,
-    //       click: function(){
-    //         global.network.someProperty="main"
-    //         const options = {
-    //           title: '提示',
-    //           message: "已切换到主网！",
-    //           buttons: ['是']
-    //         }
-    //         remote.dialog.showMessageBox(options);
-    //         var menu = Menu.buildFromTemplate(new Index().menuindex(1));
-    //         Menu.setApplicationMenu(menu);
-    //       }
+    },{
+      label: s10,
+      submenu: [{
+        label: s11, type: 'checkbox',id:'check1',checked: checka,
+          click: function(){
+            global.network.someProperty="main"
+            const options = {
+              title: '提示',
+              message: "已切换到主网！",
+              buttons: ['是']
+            }
+            remote.dialog.showMessageBox(options);
+            var menu = Menu.buildFromTemplate(new Index().menuindex(1));
+            Menu.setApplicationMenu(menu);
+          }
       
-    //   },{
-    //     label: s12, type: 'checkbox',checked: checkb,
-    //       click: function(){
-    //         global.network.someProperty="test"
-    //         const options = {
-    //           title: '提示',
-    //           message: "已切换到测试网！",
-    //           buttons: ['是']
-    //         }
-    //         remote.dialog.showMessageBox(options);
-    //         var menu = Menu.buildFromTemplate(new Index().menuindex(2));
-    //         Menu.setApplicationMenu(menu);
-    //       }
+      },{
+        label: s12, type: 'checkbox',checked: checkb,
+          click: function(){
+            global.network.someProperty="test"
+            const options = {
+              title: '提示',
+              message: "已切换到测试网！",
+              buttons: ['是']
+            }
+            remote.dialog.showMessageBox(options);
+            var menu = Menu.buildFromTemplate(new Index().menuindex(2));
+            Menu.setApplicationMenu(menu);
+          }
       
-    //   }]
-    // }
-    ,{
+      }]
+    },{
       label: s6,
       submenu: [{
         label: s7,
